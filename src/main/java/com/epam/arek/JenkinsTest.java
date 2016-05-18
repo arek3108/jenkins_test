6b67ce83b1ccdbb205503dd20a1b5d4c45a3b428
Arkadiusz Sarzynski <Arkadiusz_Sarzynski@epam.com>
2016-05-18 11:51:17 +0200

    initial commit

---
+++ src/main/java/com/epam/arek/JenkinsTest.java
@@ -0,0 +1,10 @@
+package com.epam.arek;
+
+/**
+ * Created by Arkadiusz_Sarzynski on 5/18/2016.
+ */
+public class JenkinsTest {
+    public static void main(String[] argv) {
+        System.out.println("Jenkins Test");
+    }
+}
